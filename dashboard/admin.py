1116f2cfd367a709fc346ea7888cbf8225bcf483
shhweta210 <shhweta021@gmail.com>
2026-04-04 17:07:31 +0530

    add company wise sequence filtering

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,8 +1,20 @@
 from django.contrib import admin
 
-from dashboard.models import Alerts,Logistic,TallyIpAdress
+from dashboard.models import Alerts,Logistic,TallyIpAdress,SequenceSettings
 
 # Register your models here.
+
+
+
+class SequenceSettingsList(admin.ModelAdmin):
+    list_display = ['voucher_type','from_date','prefix','suffix', 'zero_length','skip_count','is_active']
+    list_filter = ('voucher_type',)
+    list_per_page = 100
+
+
+    class Meta:
+        fields = "__all__"
+admin.site.register(SequenceSettings,SequenceSettingsList)
 
 admin.site.register(Alerts)
 admin.site.register(Logistic)
