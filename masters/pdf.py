f7a0011ea16cb1a4bfc35821a1038a6cb1e0093b
shhweta210 <shhweta021@gmail.com>
2026-04-10 16:07:25 +0530

    update awb pdf

--- masters/pdf.py
+++ masters/pdf.py
@@ -475,7 +475,7 @@ def AWB_pdf(request,id):
             try:
                 
                 logo = f'{air_mbl.company_type.mbl_final_image.url}' 
-                c.drawImage(logo, 300, 665, width=3.7 * inch, height=1.6 * inch)
+                c.drawImage(logo, 300, 675, width=3.7 * inch, height=1.6 * inch)
             except:
                 pass
 
